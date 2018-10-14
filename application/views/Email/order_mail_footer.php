91f5b8fab8a61e506b588568a235f6df1462eb28
dev1 <dev@octopuscart.com>
2018-10-14 08:39:39 +0530

    changes

--- application/views/Email/order_mail_footer.php
+++ application/views/Email/order_mail_footer.php
@@ -3,6 +3,6 @@
     <tr>
         <td width="33%">{DATE j-m-Y}</td>
         <td width="33%" align="center">{PAGENO}/{nbpg}</td>
-        <td width="33%" style="text-align: right;">Invoice  No.:<?php echo $order_data->order_no; ?></td>
+        <td width="33%" style="text-align: right;">Invoice  No.: <?php echo $order_data->order_no; ?></td>
     </tr>
 </table>
