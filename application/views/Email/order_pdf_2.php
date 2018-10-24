0029f94e8879c18765ce2a9a3d715da0f05b879a
dev1 <dev@octopuscart.com>
2018-10-24 19:04:17 +0530

    change

--- application/views/Email/order_pdf_2.php
+++ application/views/Email/order_pdf_2.php
@@ -26,21 +26,8 @@ As one of Hong Kong's leading custom tailors, we are proud of our international 
 
 
 <hr/>
-<div style="height: 200px;">Kindest Regards,<br />
-    <img src="<?php echo site_mail_logo; ?>" style="height: 30px;  background: #000 ;margin: 5px 0px 10px ;"><br/>
-    <span style="float: left; font-size: 12px;">
+<?php
 
-        <address style=" font-size: 12px;">
-            <b>Address 1</b><br/>
-            2nd Floor, 45 Haiphong Road,<br/> 
-            Tsim Sha Tsui, Kowloon, Hong Kong.
-            <br/><br/>
-            <b>Address 2</b><br/>
-            Shop D, Ground Floor, Hanyee Building, 19-21 Hankow Road, <br/>
-            Tsim Sha Tsui, Kowloon, Hong Kong.                                        <br/>
-             <b>Tel#</b>: +(852) 2730 8566  <b>Fax#</b>: +(852) 2730 8577<br/>
-            <b>Email</b>: info@bespoketailorshk.com  &nbsp;
-            <b>Web</b>: www.bespoketailorshk.com
-        </address>
-    </span>
-</div>
+echo email_footer;
+
+?>
--- application/views/Email/order_pdf_3.php
+++ application/views/Email/order_pdf_3.php
@@ -23,21 +23,8 @@ We have booked your appointment to see our  Tailor,  in London on Friday, 12 Dec
 On the day of your appointment, <br/>please call us on his contact no. (UK: +44 00000000) and he will give you his suite number.
 
 <hr/>
-<div style="height: 200px;">Kindest Regards,<br />
-    <img src="<?php echo site_mail_logo; ?>" style="height: 30px;  background: #000 ;margin: 5px 0px 10px ;"><br/>
-    <span style="float: left; font-size: 12px;">
+<?php
 
-        <address style=" font-size: 12px;">
-            <b>Address 1</b><br/>
-            2nd Floor, 45 Haiphong Road,<br/> 
-            Tsim Sha Tsui, Kowloon, Hong Kong.
-            <br/><br/>
-            <b>Address 2</b><br/>
-            Shop D, Ground Floor, Hanyee Building, 19-21 Hankow Road, <br/>
-            Tsim Sha Tsui, Kowloon, Hong Kong.                                        <br/>
-             <b>Tel#</b>: +(852) 2730 8566  <b>Fax#</b>: +(852) 2730 8577<br/>
-            <b>Email</b>: info@bespoketailorshk.com  &nbsp;
-            <b>Web</b>: www.bespoketailorshk.com
-        </address>
-    </span>
-</div>
+echo email_footer;
+
+?>
