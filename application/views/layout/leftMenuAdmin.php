27fa920f062467eeb19cf975762549e16382150d
dev1 <dev@octopuscart.com>
2019-02-15 15:32:55 +0530

    changes

--- application/views/layout/leftMenuAdmin.php
+++ application/views/layout/leftMenuAdmin.php
@@ -184,77 +184,7 @@ function checkPermission($session_data) {
                     </ul>
                 </li>
 
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-envelope"></i>
-                        <span>Message Book</span>
-                        <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                        </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <!--Admin Access-->
-
-                        <li>
-                            <a href="<?php echo site_url("Messages/getContactList")?>">
-                                <i class="active fa fa-plus "></i> <span>Send Mail/Newsletter</span>
-                            </a>
-                        </li>  
-                        <!--end of admin access-->
-                    </ul>
-                </li>
-
-
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-calendar"></i>
-                        <span>Schedule Management</span>
-                        <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                        </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <!--Admin Access-->
-
-                        <li>
-                            <a href="<?php echo base_url(); ?>index.php/Services/appointment">
-                                <i class="active fa fa-plus "></i> <span>Set Schedule</span>
-                            </a>
-                        </li>  
-                        <li>
-                            <a href="<?php echo base_url(); ?>index.php/Services/appointment_report">
-                                <i class="active fa fa-plus "></i> <span>Schedule Report</span>
-                            </a>
-                        </li>  
-                        <!--end of admin access-->
-                    </ul>
-                </li>
-
-
-
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-user"></i>
-                        <span>User Management</span>
-                        <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                        </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <!--Admin Access-->
-                        <li>
-                            <a href="<?php echo base_url(); ?>index.php/UserManager/addManager">
-                                <i class="active fa fa-plus "></i> <span>Add User</span>
-                            </a>
-                        </li>   
-                        <li>
-                            <a href="<?php echo base_url(); ?>index.php/UserManager/usersReportManager">
-                                <i class="active fa fa-plus "></i> <span>Users Reports</span>
-                            </a>
-                        </li>   
-                        <!--end of admin access-->
-                    </ul>
-                </li>
+               
 
 
                 <!--                <li class="treeview">
